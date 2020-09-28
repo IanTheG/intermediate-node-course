3f0655343320a2ae70b87d28430203bf329b5260
IanTheG <goodwinij1@gmail.com>
2020-09-28 14:37:46 -0500

    update mongodb with PUT request

--- server.js
+++ server.js
@@ -91,10 +91,50 @@ Find the "READ" route in our server file, and replace it with this code:
   })
 })
 
-// UPDATE
+/* UPDATE (old code)
 .put((req,res)=>{
   // User.findByIdAndUpdate()
 })
+*/
+
+// UPDATE (new code)
+/* If you want to update a document in mongoDB, you can do it with the User.findByIdAndUpdate method. 
+This takes three arguments (id, newData, callback). The id is still coming from "req.params", but newData is an object sent through the "req.body". 
+Also, by default the update method will return the unmodified document. 
+We can add an "options" argument before the callback ({new:true}) to make it return the modified document. */
+.put((req,res)=>{
+  User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    {
+      new:true
+    },
+    (err,data)=>{
+      if (err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    }
+  )
+})
+
+
 // DELETE
 .delete((req,res)=>{
   // User.findByIdAndDelete()
