797980dfae93316ef8942418b147e8d9fc707731
IanTheG <goodwinij1@gmail.com>
2020-09-29 08:56:23 -0500

    refactoring routes

--- server.js
+++ server.js
@@ -9,6 +9,19 @@
     brew services stop mongodb-community@4.4
 */ 
 
+/*
+Good Job! 👍 You are finished! But, there is still much more you can do with Node.js, Express.js, and MongoDB. 
+If you want to continue working on this, here are some next steps to improve security:
+
+    Encrypt your user passwords with a library like "bcrypt" before saving them.
+
+    Add a nested collection (activities, pets, blog posts, etc... ) to your User model using the "populate" method.
+
+    Use a library like JSON web token to authenticate your users in a login route.
+
+    Use middleware for your mongoose model or express routes to check if there is a valid token that matches the user's id before saving data.
+*/
+
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
